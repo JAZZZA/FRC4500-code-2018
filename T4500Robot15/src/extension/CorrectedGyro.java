9c63b48acc45378df689b795c68aaaeb205094fd
Aditya Cowsik <aditya.cowsik@gmail.com>
2015-01-25 11:20:42 -0600

    Corrected for the gyro insanity in the CorrectedGyro class. - This needs to be tested.

--- T4500Robot15/src/extension/CorrectedGyro.java
+++ T4500Robot15/src/extension/CorrectedGyro.java
@@ -2,25 +2,45 @@ package extension;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Timer;
 
 public class CorrectedGyro extends Gyro{
 	double correctionFactor;
+	Timer t;
+	
 	public CorrectedGyro(AnalogInput channel, double correctionFactor) {
 		super(channel);
 		this.correctionFactor = correctionFactor;
+		t = new Timer();
+		t.reset();
+		t.start();
 	}
 	
 	public CorrectedGyro(int channel, double correctionFactor){
 		super(channel);
 		this.correctionFactor = correctionFactor;
+		t = new Timer();
+		t.reset();
+		t.start();
+		
 	}
 	
+	public void reset(){
+		super.reset();
+		t.reset();
+		t.start();
+	}
+	
+	//TODO: find out if the correction works at all
+	
 	public double getAngle(){
-		return super.getAngle() * correctionFactor;
+		double time = t.get();
+		return super.getAngle() * correctionFactor - (0.0002024 * time * time + .1813 * time);	//subtract drift
 	}
 	
 	public double getRate(){
-		return super.getRate() * correctionFactor;
+		double time = t.get();
+		return super.getRate() * correctionFactor - (2 * 0002024 * time + .1813);	//subtract rate of change of drift
 	}
 
 }

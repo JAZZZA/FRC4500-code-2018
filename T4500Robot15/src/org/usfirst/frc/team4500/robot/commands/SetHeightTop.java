37be0edc4f02065f56726a59ca415dd8bf394abe
Tominator368 <thomas.schweich@gmail.com>
2015-01-29 18:10:22 -0600

    Changed from initialize to execute

--- T4500Robot15/src/org/usfirst/frc/team4500/robot/commands/SetHeightTop.java
+++ T4500Robot15/src/org/usfirst/frc/team4500/robot/commands/SetHeightTop.java
@@ -17,11 +17,11 @@ public class SetHeightTop extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.elevator.setHeightTop();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        Robot.elevator.setHeightTop();
     }
 
     // Make this return true when this Command no longer needs to run execute()

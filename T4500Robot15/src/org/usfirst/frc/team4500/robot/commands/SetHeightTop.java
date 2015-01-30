c7e51d97968353420142f2c381bfa5cb3cd0b9ea
Tominator368 <thomas.schweich@gmail.com>
2015-01-29 18:17:23 -0600

    Commented out end()

--- T4500Robot15/src/org/usfirst/frc/team4500/robot/commands/SetHeightTop.java
+++ T4500Robot15/src/org/usfirst/frc/team4500/robot/commands/SetHeightTop.java
@@ -31,7 +31,7 @@ public class SetHeightTop extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.elevator.breakElevator(true);
+    	//Robot.elevator.breakElevator(true);
     }
 
     // Called when another command which requires one or more of the same

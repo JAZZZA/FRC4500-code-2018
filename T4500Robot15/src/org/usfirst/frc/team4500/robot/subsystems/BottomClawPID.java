c4570f51aec18d56718772ecc434e5d850a360c2
Unidan <aditya.cowsik@gmail.com>
2015-02-02 16:33:28 -0600

    PID SmartDashboard displays created

--- T4500Robot15/src/org/usfirst/frc/team4500/robot/subsystems/BottomClawPID.java
+++ T4500Robot15/src/org/usfirst/frc/team4500/robot/subsystems/BottomClawPID.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team4500.robot.RobotMap;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -15,7 +16,7 @@ public class BottomClawPID extends PIDSubsystem {
 	Encoder screwEncoder;
 
     // Initialize your subsystem here
-	private static double Kp = 0, Ki = 0, Kd = 0;
+	private static double Kp = SmartDashboard.getNumber("Kp: ", 1), Ki = SmartDashboard.getNumber("Ki: ", 0), Kd = SmartDashboard.getNumber("Kd: ", 0);
     public BottomClawPID() {
     	super(Kp, Ki, Kd);
     	screwEncoder = new Encoder(RobotMap.screwDriveEncoder1, RobotMap.screwDriveEncoder2);
@@ -43,5 +44,7 @@ public class BottomClawPID extends PIDSubsystem {
     public void resetEncoder(){
     	screwEncoder.reset();
     }
+    
+    
 
 }

89445291da350ece0b5383049247ce05f7031708
Justin <justinguilak@claytonschools.net>
2016-01-23 14:33:18 -0600

    asdfasdfasdf

---
+++ T4500Robot15/src/org/usfirst/frc/team4500/robot/commands/MoveMotor.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team4500.robot.commands;
+
+import org.usfirst.frc.team4500.robot.Robot;
+import org.usfirst.frc.team4500.robot.subsystems.RandomMotor;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveMotor extends Command {
+
+    public MoveMotor() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.moveMotor.moveMotor(Robot.oi.secondStick.getY());
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ T4500Robot15/src/org/usfirst/frc/team4500/robot/subsystems/RandomMotor.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team4500.robot.subsystems;
+
+import org.usfirst.frc.team4500.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class RandomMotor extends Subsystem {
+	
+	Talon randomMotor = new Talon(RobotMap.arroundMotor);
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void moveMotor(double speed) {
+    	randomMotor.set(speed);
+    }
+}
+
